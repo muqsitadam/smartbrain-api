da60a29bc989b93c2d41ee03d85d2ec54e291fcf
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-07-16 13:47:19 +0100

    Added finishing touches to the server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db  = knex({
 });
 
 db.select('*').from('users').then(data => {
-    console.log(data)
+    // console.log(data)
 })
 
 const app =  express()
@@ -54,34 +54,56 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-    res.send(database.user)
+    res.send('success')
 })
 
 //SignIn
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.user[0].email && 
-       req.body.password === database.user[0].password){
-        res.json(database.user[0]);
-        // res.json('success')
-    }else{
-        res.status(400).json('Wrong email or password')
-    }
+    db.select('email', 'hash').from('login')
+       .where('email', '=', req.body.email)
+       .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+            if(isValid){
+                db.select('*').from('users')
+                .where('email', '=', req.body.email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('Unable to get user'))
+            }else{
+                res.status(400).json('Wrong credentials')
+            }
+       })
+       .catch(err => res.status(400).json('Wrong credentials'))
 })
 
 //Register
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body
-    db('users')
-    .returning('*')
-    .insert({
-        name: name,
-        email: email,
-        joined: new Date()
+    const hash = bcrypt.hashSync(password)
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                name: name,
+                email: loginEmail[0].email,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0])
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)   
     })
-    .then(user => {
-        res.json(user[0])
-    })
-    .catch(err => res.status(400).json('Email already exists'))
+    .catch(err => res.status(400).json('Unable to register'))
 })
 
 //Profile ID
