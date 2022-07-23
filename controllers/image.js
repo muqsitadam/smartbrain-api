c5dcf90ed42b996351365af339fb8a9ccb036be4
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-07-23 19:46:28 +0100

    Created another endpoint for image url

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+
+const apps = new Clarifai.App({
+    apiKey: 'b724e725d3674691b496eef8f4858d1f'
+})
+ 
+const handleApiCall =  (req, res) => {
+    apps.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
